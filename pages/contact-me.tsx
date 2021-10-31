8f21e8880ba2292306e01be15e383626c4891ef7
Hamid Nawaz <hamid.nawaz28@yahoo.com>
2021-10-31 20:12:12 +0500

    contact me fixes

--- pages/contact-me.tsx
+++ pages/contact-me.tsx
@@ -1,14 +1,53 @@
+import { makeStyles } from "@mui/styles";
+import { Theme } from "@mui/system";
+import { Typography } from "@mui/material";
+import { Footer } from "../containers/footer";
+import { Seo } from "../components/seo";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    textAlign: "center",
+    padding: "5% 20%",
+    [theme.breakpoints.down("sm")]: {
+      padding: "5% 2%",
+    },
+  },
+}));
+
+const contactDetails = [
+  {
+    label: "Email",
+    info: "info@hamidnawaz.com",
+  },
+  {
+    label: "Phone",
+    info: "+923125012601",
+  },
+];
+
 const ContactMe = () => {
+  const { root } = useStyles();
   return (
     <div>
-      <div>
-        <h1>Email</h1>
-        <p>info@hamidnawaz.com</p>
-      </div>
-      <div>
-        <h1>Phone</h1>
-        <p>+923125012601</p>
-      </div>
+      <Seo
+        description="Contact me page of hamid nawaz"
+        pageTitle="Contact Me"
+        contentTitle="Contact Hamid Nawaz"
+        pageUrl="/contact-me"
+      />
+      <main>
+        <div className={root}>
+          {contactDetails.map((el, id) => {
+            return (
+              <div key={id}>
+                <Typography variant="h3">{el.label}</Typography>
+                <p>{el.info}</p>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
